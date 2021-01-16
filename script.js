1ac6e1c5f1e439dfc78bd144edd67690c181ed6c
vladislav boldakov <boldakov.vladislav@gmail.com>
2021-01-16 23:54:57 +0700

    feat: improve editor
    add sanitizer for nodes and editor
    add coping headers to MS Word

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttons = document.querySelector('.toolkit').querySelectorAll('button');
+const editor = document.querySelector('.edit-area');
+
+const header1Style = `font-size: 24px;`;
+const header2Style = `font-size: 19px;`;
 
 const isEmptySelection = (selection) => !Boolean(selection.toString());
 const getClass = (btnClass) => {
@@ -12,10 +16,56 @@ const getClass = (btnClass) => {
         case 'italic':
             return 'italic-text'
     }
-}
+};
 const insertNode = (range, element) => {
     range.deleteContents();
     range.insertNode(element);
+};
+const createElement = (className) => {
+    let newElement;
+    switch (className) {
+        case 'header1-text':
+            newElement = document.createElement('h1');
+            newElement.setAttribute("style", header1Style);
+            break;
+        case 'header2-text':
+            newElement = document.createElement('h2');
+            newElement.setAttribute("style", header2Style);
+            break;
+        default:
+            newElement = document.createElement('span');
+            break;
+    }
+    newElement.classList.add(className);
+    return newElement;
+};
+const sanitizeNodesWithSameClass = (nodeListWithSameClass) => {
+    nodeListWithSameClass.forEach( (node) => {
+        node.className = '';
+    });
+};
+const sanitizeGarbageTags = (newElement) => {
+    const spanNodeList = newElement.querySelectorAll('*');
+    /** replace tags without styling class to plain/text */
+    spanNodeList.forEach( (node) => {
+        if (node.className === '') {
+            const parent = node.parentNode;
+            while( node.firstChild ) {
+                parent.insertBefore(  node.firstChild, node );
+            }
+            parent.removeChild( node );
+        }
+    });
+};
+const sanitizeEditorGarbageTags = () => {
+    const allTags = editor.querySelectorAll('*');
+    /** remove empty tags */
+    allTags.forEach((node) => {
+        if (node.nodeType !== 3 && node.innerHTML === '') {
+            node.remove();
+        }
+    });
+    editor.normalize();
 }
 
 buttons.forEach((btn) => {
@@ -23,27 +73,28 @@ buttons.forEach((btn) => {
     btn.addEventListener('click', () => {
         if (document.getSelection) {
             const selection = document.getSelection();
+
             if (isEmptySelection(selection)) {
                 return;
             }
 
             const range = selection.getRangeAt(0);
+            /** oldContent - #document-fragment */
             const oldContent = range.extractContents();
-            const firstChild = oldContent.firstElementChild;
-            const customizingClass = getClass(btn.className);
 
-            if (firstChild && firstChild.className === customizingClass) {
-                /** not reliable unset styling */
-                const elementWithoutStyling = firstChild.innerHTML;
-                const template = document.createElement('template');
-                template.innerHTML = elementWithoutStyling;
-                insertNode(range, template.content.firstChild);
-            } else {
-                const newElement = document.createElement('span');
-                newElement.classList.add(customizingClass);
-                newElement.appendChild(oldContent);
-                insertNode(range, newElement);
-            }
+            const customizingClass = getClass(btn.className);
+            const rootSpan = document.createElement('span');
+            rootSpan.appendChild( oldContent.cloneNode(true) );
+
+            sanitizeNodesWithSameClass(rootSpan.querySelectorAll(`.${customizingClass}`));
+
+            const newElement = createElement(customizingClass);
+            newElement.appendChild(rootSpan);
+
+            sanitizeGarbageTags(newElement);
+
+            insertNode(range, newElement);
+            sanitizeEditorGarbageTags();
         }
     })
-})
+});
